ab9e92fd3c83524794512d73a4b030288f422e98
Mathix <mathieu.tessier9@gmail.com>
2020-05-22 20:30:22 +0200

    Log messages

--- mylar/webserve.py
+++ mylar/webserve.py
@@ -955,7 +955,7 @@ class WebInterface(object):
     def fillPageCount(self, publisher):
         myDB = db.DBConnection()
         comics = myDB.select('SELECT * FROM comics WHERE ComicPublisher=?', [publisher])
-
+        counter = 0
         # comicid = str(comicid)  # it's coming in unicoded...
 
         # logger.fdebug(type(comicid))
@@ -980,13 +980,19 @@ class WebInterface(object):
 
                 metainfo = helpers.IssueDetails(filelocation, IssueID=issueid)
 
+                if metainfo['metadata'] is None:
+                    logger.info('No metadata found for issueid:' + issue['IssueID'])
+                    continue
+
                 pg_count = metainfo['metadata']['pagecount']
 
                 controlValueDict = {'IssueID': issueid}
                 newValueDict = {'PageCount': pg_count}
                 myDB.upsert("issues", newValueDict, controlValueDict)
+                counter += 1
+                logger.info('Pagecount filled for issueid:' + issue['IssueID'])
 
-                logger.fdebug('issueid:' + issue['IssueID'])
+            logger.info(f'{counter} issues have been processed')
     fillPageCount.exposed = True
 
     def pauseSeries(self, ComicID):
